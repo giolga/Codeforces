c70f9ccfdfe12638b5b1e2ce94103054b21fd7a2
giolga <giolga.kumi@gmail.com>
2024-07-11 11:50:31 +0400

    first commit

---
+++ Petya_and_strings/Petya_and_strings/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Petya_and_strings
+{
+    internal class Program
+    {
+        private static bool StringCompare(string s1, string s2)
+        {
+            int size = 0;
+            while (size < s1.Length)
+            {
+                if (Convert.ToInt32(s1[size]) < Convert.ToInt32(s2[size]))
+                {
+                    return false;
+                }
+                if (Convert.ToInt32(s2[size]) < Convert.ToInt32(s1[size]))
+                {
+                    return true;
+                }
+
+                size++;
+            }
+
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            string s1 = Console.ReadLine().ToLower();
+            string s2 = Console.ReadLine().ToLower();
+
+            if (s1 == s2)
+            {
+                Console.WriteLine(0);
+            }
+            else
+            {
+                if (!StringCompare(s1, s2))
+                {
+                    Console.WriteLine(-1);
+                }
+                else
+                {
+                    Console.WriteLine(1);
+                }
+            }
+
+            //Console.ReadKey();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Codeforceshttps://codeforces.com/problemset/problem/112/A
+# Codeforces https://codeforces.com/problemset/problem/112/A
