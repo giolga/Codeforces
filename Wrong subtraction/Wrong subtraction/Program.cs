00519b251531dcffe5b491b43abda9128ab43815
giolga <giolga.kumi@gmail.com>
2024-07-12 23:39:53 +0400

    Wrong subtraction

---
+++ Wrong subtraction/Wrong subtraction/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wrong_subtraction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            int n = int.Parse(input[0]), k = int.Parse(input[1]);
+
+            while (k > 0)
+            {
+                if (n % 10 != 0)
+                {
+                    n--;
+                }
+                else
+                {
+                    n /= 10;
+                }
+
+                k--;
+            }
+
+
+            Console.WriteLine(n);
+            //Console.ReadKey();
+        }
+    }
+}
