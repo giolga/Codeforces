5f4e28dad02e9a303d433be09cb70ef0f82b4a2a
giolga <giolga.kumi@gmail.com>
2024-07-13 00:20:33 +0400

    Fox and snake

---
+++ Fox and snake/Fox and snake/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fox_and_snake
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] s = Console.ReadLine().Split();
+
+            int n = int.Parse(s[0]), m = int.Parse(s[1]);
+
+            bool left = false;
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    for (int j = 1; j <= m; j++)
+                    {
+                        Console.Write('#');
+                    }
+                    Console.WriteLine();
+                    continue;
+                }
+                else
+                {
+                    if (!left)
+                    {
+                        for (int j = 1; j < m; j++)
+                        {
+                            Console.Write('.');
+                        }
+
+                        left = true;
+                        Console.WriteLine('#');
+                        continue;
+                    }
+                    else
+                    {
+                        Console.Write('#');
+                        for(int j = 2; j <= m; j++)
+                        {
+                            Console.Write('.');
+                        }
+
+                        Console.WriteLine();
+                        left = false;
+                        continue ;
+                    }
+                }
+            }
+
+            //Console.ReadKey();
+        }
+    }
+}
