97e8c06a43137b8ebe13b40686b54bdec6f5ce7c
giolga <giolga.kumi@gmail.com>
2024-07-13 20:06:10 +0400

    2 task added

---
+++ Hulk/Hulk/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hulk
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string ans = "";
+
+            int n = int.Parse(Console.ReadLine());
+            bool disdain = true;
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (disdain)
+                {
+                    ans += "I hate ";
+                    disdain = false;
+                }
+                else
+                {
+                    ans += "I love ";
+                    disdain = true;
+                }
+
+                if (i == n)
+                {
+                    ans += "it ";
+                }
+                else
+                {
+                    ans += "that ";
+                }
+
+            }
+
+            Console.WriteLine(ans);
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ I Wanna Be the Guy/I Wanna Be the Guy/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I_Wanna_Be_the_Guy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            string[] s1 = Console.ReadLine().Split(' ');
+            string[] s2 = Console.ReadLine().Split(' ');
+
+            int x = int.Parse(s1[0]);
+            int y = int.Parse(s2[0]);
+
+            SortedSet<int> levels = new SortedSet<int>();
+
+            for (int i = 1; i < s1.Length; i++)
+            {
+                levels.Add(int.Parse(s1[i]));
+            }
+
+            for (int i = 1; i < s2.Length; i++)
+            {
+                levels.Add(int.Parse(s2[i]));
+            }
+
+            if (levels.Count == n)
+            {
+                Console.WriteLine("I become the guy.");
+            }
+            else
+            {
+                Console.WriteLine("Oh, my keyboard!");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
