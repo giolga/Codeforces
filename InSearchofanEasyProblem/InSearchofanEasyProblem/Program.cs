acec22253424a4a32f3fabd5172d4996c043aa17
giolga <giolga.kumi@gmail.com>
2024-07-16 00:56:59 +0400

    Two tasks solved :)

---
+++ InSearchofanEasyProblem/InSearchofanEasyProblem/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InSearchofanEasyProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            sbyte n = sbyte.Parse(Console.ReadLine());
+            string[] s = Console.ReadLine().Split(' ');
+
+            sbyte ans = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                ans ^= sbyte.Parse(s[i]);
+                if (ans == 1)
+                {
+                    Console.WriteLine("Hard".ToLower());
+                    return;
+                }
+                
+            }
+
+            Console.WriteLine("Easy".ToLower());
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Presents/Presents/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presents
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] s = Console.ReadLine().Split(' ');
+
+            Dictionary<int, int> presents = new Dictionary<int, int>();
+
+            for (int i = 1; i <= n; i++)
+            {
+                presents.Add(int.Parse(s[i - 1]), i);
+            }
+
+            var sortedByValue = presents.OrderBy(kvp => kvp.Key).ToList();
+
+            foreach( var kvp in sortedByValue)
+            {
+                Console.Write($"{kvp.Value} ");
+            }
+
+            //Console.ReadKey();
+        }
+    }
+}
