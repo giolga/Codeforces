e3f6fe4d8f7b33813f5cb6a200804ebe76563c63
giolga <giolga.kumi@gmail.com>
2024-10-03 22:01:47 +0400

    Eratosthene Sieve

---
+++ AlgorithmsC#/Cieve/Cieve/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eratosthene_Sieve
+{
+    internal class Program
+    {
+        static int[] arr = new int[1000001];
+        static int n;
+
+        static void Sieve()
+        {
+            for(int i = 2; i <=n; i++)
+            {
+                if (arr[i] > 0)
+                    continue;
+                else
+                {
+                    for(int j = i*i; j <=n; j += i)
+                    {
+                        if(arr[j] == 0)
+                        {
+                            arr[j] = i;
+                        }
+                    }
+                }
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+            n = int.Parse(Console.ReadLine());
+
+            Sieve();
+
+            Console.ReadKey();
+        }
+    }
+}
