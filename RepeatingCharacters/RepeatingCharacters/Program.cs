b5a6be64828efcd9a8b964b979526e2ede8d9792
giolga <giolga.kumi@gmail.com>
2024-10-03 21:16:53 +0400

    Repeating characters

---
+++ RepeatingCharacters/RepeatingCharacters/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepeatingCharacters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            List<char> chars = new List<char>();
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (chars.Contains(str[i]))
+                {
+                    Console.WriteLine($"'{str[i]}'");
+                    Console.ReadKey();
+                    return;
+                }
+
+                chars.Add(str[i]);
+            }
+
+            Console.WriteLine("No repeating characters");
+            Console.ReadKey();
+        }
+    }
+}
